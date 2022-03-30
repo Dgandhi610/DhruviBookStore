bb5984529a66c684f09ec61b9f41b1c7f315a160
Dgandhi610 <90396629+Dgandhi610@users.noreply.github.com>
2022-03-30 13:16:23 -0400

    Categories table added to database.

--- DhruviBookStore.Models/Category.cs
+++ DhruviBookStore.Models/Category.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
+
 
 namespace DhruviBookStore.Models
 {
-    class Category
+    public class Category
     {
-        public int ID { get; set; }
+        [Key]
 
-        [Display(Name
+        public int Id { get; set; }
+
+        [Display(Name ="Category Name")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
     }
 }
