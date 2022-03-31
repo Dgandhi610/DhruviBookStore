eddfaf5b329e432cee07920cabb630273bbe3718
Dgandhi610 <90396629+Dgandhi610@users.noreply.github.com>
2022-03-31 01:04:49 -0400

    added category repository.

---
+++ DhruviBookStore.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,28 @@
+﻿using DhruviBookStore.DataAccess.Repository.IRepository;
+using DhruviBookStore.Models;
+using DhruviBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace DhruviBookStore.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db): base(db)
+        {
+            _db = db;
+        }
+        public void Update(Category category)
+        {
+            var objFromDb = _db.Categories.FirstOrDefault(s => s.Id == category.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = category.Name;
+                _db.SaveChanges();
+            }
+        }
+    }
+}
--- DhruviBookStore.DataAccess/Repository/Repository.cs
+++ DhruviBookStore.DataAccess/Repository/Repository.cs
@@ -3,29 +3,49 @@ using DhruviBookStore.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
+using System.Linq.Expressions;
+using System.Linq;
 
 namespace DhruviBookStore.DataAccess.Repository
 {
-    public class Repository<T> : IRepository<T> where T:class
+    public class Repository<T> : IRepository<T> where T : class
     {
-        //modidfying database with db context
-        private readonly ApplicationDbContext _db;
-        internal DbSet<T> dbSet;
         public Repository(ApplicationDbContext db)
         {
-            db = db;
-            this.dbSet = _db.Set<T>();
         }
+
         public void Add(T entity)
-public T Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(int id)
         {
             throw new NotImplementedException();
         }
 
         public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
         {
             throw new NotImplementedException();
         }
