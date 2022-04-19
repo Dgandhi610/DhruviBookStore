b9ad0a3f25d4421719c100edfc1f4b331b6f1474
Dgandhi610 <90396629+Dgandhi610@users.noreply.github.com>
2022-04-19 00:18:36 -0400

    facing 17 errors in index.cshtml general file.

--- DhruviBookStore/Areas/Customer/Controllers/HomeController.cs
+++ DhruviBookStore/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using DhruviBookStore.Models;
+﻿using DhruviBookStore.DataAccess.Repository.IRepository;
+using DhruviBookStore.Models;
 using DhruviBookStore.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -8,21 +9,24 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DhruviBookStore.Area.Customer.Controllers
+namespace DhruviBookStore.Ares.Customer.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unifOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unifOfWork)
         {
             _logger = logger;
+            _unifOfWork = unifOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unifOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+            return View(productList);
         }
 
         public IActionResult Privacy()
